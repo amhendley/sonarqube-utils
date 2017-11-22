779b8f815d7faa7579b0b15ed1449a4a3625536d
A M Hendley <ahendley@internode.on.net>
2017-11-22 20:48:10 +1100

    Refactored script sq-list-pligins.py to use argparse instead of getopt.

--- sq-list-plugins.py
+++ sq-list-plugins.py
@@ -1,41 +1,33 @@
 #!python
 
-import getopt
+import argparse
+import os
 
 from common.sonarcube import *
 
 
-def print_help():
-    print('usage: {} [-i] [-p] [-u] [-a] [-h]'.format(__file__))
-
-
-show_installed = False
-show_pending = False
-show_updates = False
-show_available = False
-sys_args = sys.argv[1:]
-headers = {'Content-Type': 'application/json'}
+parser = argparse.ArgumentParser(prog=os.path.basename(__file__),
+                                 description='Utility script to create a new project in SonarQube')
 
 try:
-    opts, args = getopt.getopt(sys_args, "hipua", ["help", "installed", "pending", "updates", "available"])
-except getopt.GetoptError:
-    print_help()
-    sys.exit(const.SYS_ERROR_INVALID_ARGS)
+    parser.add_argument('-i', '--installed', action='store_true',
+                        help='List all the plugins installed, sorted by plugin name.')
+    parser.add_argument('-p', '--pending', action='store_true',
+                        help='List plugins which will either be installed or removed at the next startup, sorted by plugin name.')
+    parser.add_argument('-u', '--updates', action='store_true',
+                        help='List plugins installed which at least one newer version is available, sorted by plugin name.')
+    parser.add_argument('-a', '--available', action='store_true',
+                        help='List all the plugins available for installation, sorted by plugin name..')
 
-for opt, arg in opts:
-    if opt in ('-h', '--help'):
-        print_help()
-        sys.exit()
-    elif opt in ("-i", "--installed"):
-        show_installed = True
-    elif opt in ("-p", "--pending"):
-        show_pending = True
-    elif opt in ("-u", "--updates"):
-        show_updates = True
-    elif opt in ("-a", "--available"):
-        show_available = True
+    args = parser.parse_args()
+except:
+    parser.print_help()
+    sys.exit()
 
-
+show_installed = args.installed
+show_pending = args.pending
+show_updates = args.updates
+show_available = args.available
 show_all = (True if not show_available and not show_updates and not show_pending and not show_installed else False)
 
 if show_all:
